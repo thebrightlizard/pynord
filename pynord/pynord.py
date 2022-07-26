49eef4e576100f2f0793d7a04cf6a89f2beab401
maria.gonzalez5884@outlook.com <maria.gonzalez5884@lookout.com>
2022-07-26 18:22:56 -0500

    major release 2.0

--- pynord/pynord.py
+++ pynord/pynord.py
@@ -1,4 +1,6 @@
-from pynord.util import util
+from pynord.util import util, parser
+from pprint import pprint
+import pandas as pd
 import subprocess
 import random
 import os
@@ -11,15 +13,16 @@ class PyNord():
 	def status(self):
 		raw_out = util.system(self, command='nordvpn status')
 		util.print(self, raw_out)
-		status = util.parse(self, string=raw_out)
+		status = parser.status(self, string=raw_out)
 		return status
-
-	# TODO: add double vpn functionality
-	def connect(self, server=False, timeout=10):
-		if server:
-			log = util.system(self, command='nordvpn connect {server}', timeout=timeout)
+		
+	def connect(self, server="The_Americas", rand=False, timeout=10):
+		if rand:
+			city_list = self.cities()
+			city = random.choice(city_list)
+			log = util.system(self, command=f'nordvpn connect {city}', timeout=timeout)
 		else:
-			log = util.system(self, command='nordvpn connect', timeout=timeout)
+			log = util.system(self, command=f'nordvpn connect {server}', timeout=timeout)
 		if self.status() is False:
 			raise Exception('ERROR: VPN Connection Failed.')
 		util.print(self, log)
@@ -35,12 +38,27 @@ class PyNord():
 			log = util.system(self, command='nordvpn set killswitch on')
 		util.print(self, log)
 
+	def cities(self, country="united_states"):
+		raw_out = util.system(self, command=f'nordvpn cities {country}')
+		cities = parser.available_cities(self, string=raw_out)
+		return cities
+
+	# TODO: parse status and retrieve "us8526" from Current server: us8526.nordvpn.com
+	def info(self):
+		raw_out = util.system(self, command='nordvpn status')
+		server = parser.server(self, string=raw_out)
+		ip = parser.ip(self, string=raw_out)
+		city = parser.city(self, string=raw_out)
+		time_zone = util.timezone(self, city=city)
+
+		info = {
+			"server": server,
+			"ip": ip,
+			"city": city,
+			"timezone": time_zone
+		}
+		return info
+
 	# TODO: add login and install functionality
 	def login(self):
 		return
-
-
-
-
-
-
--- pynord/util.py
+++ pynord/util.py
@@ -1,8 +1,12 @@
+import subprocess as sp
+import pandas as pd
 import subprocess
 import random
 import json
 import sys
 import os
+
+# NOTE: Need to update "nord_cities" every-so-often. Last update: 07/15/2022
 
 printer = print
 class helper:
@@ -18,9 +22,8 @@ class helper:
 	def print(message):
 		printer(message)
 
-class util():
-	def parse(self, string):
-		# parsing nordvpn status
+class parser():
+	def status(self, string):
 		if 'Status' in string:
 			string = string[string.find('Status:'):]
 			string = string.split('\n')
@@ -29,15 +32,34 @@ class util():
 			else:
 				status = True
 			return status
-		# parsing nordvpn --version
-		elif 'NordVPN Version' in string:
-			version = string.split()[2]
-			return True
-		elif 'command not found' in string:
-			return False
-		else:
-			pass
 
+	def server(self, string):
+		if "Current server" in string:
+			string = string[string.find('Current server:'):]
+			string = string.split('\n')[0]
+			server = string.split(": ")[1].split(".")[0]
+			return server
+			
+	def ip(self, string):
+		if "Current server" in string:
+			string = string[string.find('Server IP:'):]
+			string = string.split('\n')[0]
+			ip = string.split(": ")[1]
+			return ip
+
+	def city(self, string):
+		if "Current server" in string:
+			string = string[string.find('City:'):]
+			string = string.split('\n')[0]
+			city = string.split(": ")[1].replace(" ", "_")
+			return city
+
+	def available_cities(self, string):
+		nord_cities = ['Atlanta', 'Charlotte', 'Dallas', 'Kansas_City', 'Manassas', 'New_York', 'Saint_Louis', 'San_Francisco', 'Buffalo', 'Chicago', 'Denver', 'Los_Angeles', 'Miami', 'Phoenix', 'Salt_Lake_City', 'Seattle']
+		intersection = list(set(string.split()) & set(nord_cities))
+		return intersection
+
+class util():
 	def check_install(self):
 		# verify linux
 		if sys.platform not in ['linux', 'linux2']:
@@ -59,8 +81,29 @@ class util():
 		log = helper.run(command=command, timeout=timeout)
 		return log
 
+	def timezone(self, city):
+	    zones = {
+	    'Atlanta': "America/New_York", 
+	    'Charlotte': "America/New_York", 
+	    'Dallas': "America/Indiana/Indianapolis", 
+	    'Kansas_City': "America/Indiana/Indianapolis", 
+	    'Manassas': "America/New_York", 
+	    'New_York': "America/New_York", 
+	    'Saint_Louis': "America/Indiana/Indianapolis", 
+	    'San_Francisco': "America/Los_Angeles", 
+	    'Buffalo': "America/New_York", 
+	    'Chicago': "America/Chicago", 
+	    'Denver': "America/Denver", 
+	    'Los_Angeles': "America/Los_Angeles", 
+	    'Miami': "America/New_York", 
+	    'Phoenix': "America/Phoenix", 
+	    'Salt_Lake_City': "America/Phoenix", 
+	    'Seattle': "America/Los_Angeles"
+	    }
 
+	    try:
+	        zone = zones[city]
+	    except:
+	        zone = "America/Los_Angeles"
 
-
-
-			
+	    return zone
